56f21a17b07e71db4241a3a26a67c6f7f586aac5
Andrew Kouré <andrew@koure.org>
2026-06-23 14:42:55 +0000

    Updated tests

--- tests/testthat/test-parameterized.R
+++ tests/testthat/test-parameterized.R
@@ -13,7 +13,7 @@ test_that("tm_ridership sends start_date, end_date as query params", {
 test_that("tm_ridership sends line_id when provided", {
   httr2::local_mocked_responses(
     function(req) {
-      expect_true(grepl("line_id=line-red", req$url))
+      expect_true(grepl("line_id=line-Red", req$url))
       mock_response('[]')
     }
   )
@@ -34,7 +34,7 @@ test_that("tm_line_delays sends required params", {
   httr2::local_mocked_responses(
     function(req) {
       expect_true(grepl("linedelays", req$url))
-      expect_true(grepl("line=line-red", req$url))
+      expect_true(grepl("line=Red", req$url))
       mock_response('[]')
     }
   )
@@ -67,7 +67,7 @@ test_that("tm_speed_restrictions sends line_id and on_date", {
   httr2::local_mocked_responses(
     function(req) {
       expect_true(grepl("speed_restrictions", req$url))
-      expect_true(grepl("on_date=2024-01-15", req$url))
+      expect_true(grepl("date=2024-01-15", req$url))
       mock_response('[]')
     }
   )
